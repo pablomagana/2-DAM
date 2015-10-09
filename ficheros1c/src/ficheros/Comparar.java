42ddbf442c690fbf93e488461c1ec2e1a5d7e266
pablo <pablomagub@gmail.com>
2015-10-09 23:08:22 +0200

    Signed-off-by: pablo <pablomagub@gmail.com>

--- ficheros1c/src/ficheros/Comparar.java
+++ ficheros1c/src/ficheros/Comparar.java
@@ -11,6 +11,7 @@ public class Comparar {
 	public static void main(String[] args) {
 		File file1=new File("t1.txt");
 		File file2=new File("t2.txt");
+		//comprobar si los archivos son o no iguales y mostrar error si se produce cualquier error
 		try{
 			if(comparar(file1,file2)==true){
 				System.out.println("los archivos SON iguales");
@@ -23,6 +24,8 @@ public class Comparar {
 		}
 		
 		File file3=new File("t3.txt");
+		//buscar una referencia a la palabra a buscar en el archivo guardando la linea de la primera o ultima referencia
+		// mostrando mensaje de error si se produce cualquier error
 		try{
 			System.out.println("la palabra se encuentra en la linea "+buscarPalabra(file3, "casa", false));
 		}catch(Exception e){
@@ -32,20 +35,26 @@ public class Comparar {
 	}
 	
 	public static boolean comparar(File file1,File file2) throws IOException{
+		//crear flujo de datos
 		BufferedReader br1=new BufferedReader(new FileReader(file1));
 		BufferedReader br2=new BufferedReader(new FileReader(file2));
-		
+		//leer linea inicial
 		String s1=br1.readLine();
 		String s2=br2.readLine();
-		
+		//recorro linea a linea comprobando si son iguales
+		//si no lo son se corta el bucle y devuelve false
 		while(s1!=null && s2!=null){
 			if(s1.equals(s2)){
 				s1=br1.readLine();
 				s2=br2.readLine();
 			}else{
+				//cierro archivos
+				br1.close();
+				br2.close();
 				return false;
 			}
 		}
+		//cierro archivos
 		br1.close();
 		br2.close();
 		return true;
@@ -53,11 +62,14 @@ public class Comparar {
 	}
 	
 	public static int buscarPalabra (File fichero1, String palabra, boolean primera) throws IOException{
+		//creo flujo de datos
 		BufferedReader br1=new BufferedReader(new FileReader(fichero1));
+		//leer linea inicial
 		String s=br1.readLine();
+		//inicializa variables
 		int linea=1;
 		int lineaEncontrada=0;
-		
+		//recorro linea a linea y dependiendo si busco primera linea o no comparo la palabra leida con la buscada y la guardo en variable
 		while(s!=null){
 			if(primera==true && s.equals(palabra)){
 				lineaEncontrada=linea;
@@ -70,6 +82,7 @@ public class Comparar {
 			linea++;
 			s=br1.readLine();
 		}
+		//cerrar archivo
 		br1.close();
 		return lineaEncontrada;
 	}
