3f102b4757f06a6e81a02227bb289109a9d79d9e
pablo <pablomagub@gmail.com>
2015-10-16 18:08:54 +0200

    Signed-off-by: pablo <pablomagub@gmail.com>

--- FlujoDeBytes(OP)Imagen/Principal/Main.java
+++ FlujoDeBytes(OP)Imagen/Principal/Main.java
@@ -15,27 +15,85 @@ import javax.imageio.ImageWriter;
 import org.omg.PortableInterceptor.IORInfo;
 
 public class Main {
-	static BufferedImage imagenorigen=null;
-
+	/*
+	 * Pablo Magaña
+	 */
+	static BufferedImage bufferedorigen=null;
+	static BufferedImage buffereddestino=null;
+	static File destino;
+	static File origen;
+	static String menu;
+	
 	public static void main(String[] args) {
+		
 		Scanner s=new Scanner(System.in);
+		System.out.println("nombre archivo origen:");
 		String nameOrigen=s.nextLine();
-		File origen=new File(nameOrigen);
+		System.out.println("nombre archivo destino:");
+		String nameDestino=s.nextLine();
+		origen=new File(nameOrigen);
+		destino=new File(nameDestino);
+		System.out.println("elige una opción apara aplicar a la imagen");
+		System.out.println("1 - para rotar la imagen 90º");
+		System.out.println("2 - para hacer su espejo");
+		menu=s.nextLine();
 		try {
-			imagenorigen=ImageIO.read(origen);
+			bufferedorigen=ImageIO.read(origen);
+			buffereddestino=new BufferedImage(bufferedorigen.getWidth(), bufferedorigen.getHeight(), bufferedorigen.getType());
+			
 		} catch (IOException e) {
 			System.out.println("error al abrir fichero origen");
+		}
+		try{
+			if(menu.equals("1")){
+				rotar();
+			}else if(menu.equals("2")){
+				espill();
+			}else{
+				System.out.println("opción no valida, vuelve a intentarlo");
+			}
+		}catch(IOException e){
+			System.out.println("error al rotar la imagen");
 		}
 	}
 	
 	
-	public void rotar(){
-		for(int i=0;i<imagenorigen.getMinX();i++){
-			for(int j=0;j<imagenorigen.getMinY();j++){
-				imagenorigen.getRGB(i, j);
-			}
+	public static void rotar() throws IOException{
+		
+		
+		int ancho = bufferedorigen.getWidth();//512
+		int alto = bufferedorigen.getHeight();//512
+		
+		int rgbo;//origen
+		int rgbo2;
+		for(int i=0;i<ancho;i++){
+			for(int j=0;j<alto;j++){
+				rgbo=bufferedorigen.getRGB(i, j);
 				
+				rgbo2=bufferedorigen.getRGB(j, (ancho-1)-i);
+			
+				buffereddestino.setRGB((ancho-1)-j,i , rgbo);
+				buffereddestino.setRGB(i,j,rgbo2);
+			}
+			
 		}
-		System.out.println("imagen leida");
+		System.out.println("imagen rotada 90º a la derecha");
+		ImageIO.write(buffereddestino, "png", destino);
+	}
+	
+	public static void espill() throws IOException{
+		int ancho = bufferedorigen.getWidth();
+		int alto = bufferedorigen.getHeight();
+		
+		int rgbo;//origen
+		for(int i=0;i<ancho;i++){
+			for(int j=0;j<alto;j++){
+				rgbo=bufferedorigen.getRGB(i, j);
+				buffereddestino.setRGB((ancho-1)-i,j, rgbo);
+			}
+		}
+		
+		System.out.println("imagen espejo");
+		ImageIO.write(buffereddestino, "jpg", destino);
 	}
 }
