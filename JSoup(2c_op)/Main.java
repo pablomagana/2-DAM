37c986fd0d01049db97d89bf035899cda6e4413f
Pablo <pablomagub@gmail.com>
2015-11-06 21:36:19 +0100

    Jsoup 2c terminado comentarios
    Signed-off-by: Pablo <pablomagub@gmail.com>

--- JSoup(2c_op)/Main.java
+++ JSoup(2c_op)/Main.java
@@ -15,17 +15,17 @@ public class Main {
 	public static void main(String[] args) {
 		//http://www.pccomponentes.com/
 		
-		
-		
 		Document html=null;
-		try{
+		try{//conecto con el documento html en red y lodescargo
 			html=Jsoup.connect("http://www.pccomponentes.com").get();
 		}catch(Exception e){
 			
 		}
+		//capturo el titulo del html
 		System.out.println(html.title());
 		System.out.println("");
 		
+		//obtener lista con todos los elementos de los productos y otra paa los precios
 		ArrayList<Element> articulosPrecio=html.getElementsByClass("preciosProductos");
 		ArrayList<Element> articulosNombre=html.getElementsByClass("nombre");
 		//al extraerse los nombre y los precios de los productos con el mismo orden los puedo relacionar
@@ -33,18 +33,14 @@ public class Main {
 		//iterador productos
 				Iterator it2=articulosNombre.iterator();
 		//iterador precios
+				//se recorren a la par los dos iteradores y se imprime el producto junto con el precio
 				Iterator it=articulosPrecio.iterator();
 				while(it2.hasNext()){
 					Element e2=(Element) it2.next();
 					Element e=(Element) it.next();
 					System.out.println("Contenido: "+e2.text()+" - "+" Precio: "+e.text());
 				}
-		/*
 		
-		while(it.hasNext()){
-			Element e=(Element) it.next();
-			System.out.println(e.text());
-		}*/
 		
 	}
 
