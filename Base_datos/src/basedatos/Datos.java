dd724344eec71c909da904e7280f785d382738cf
pablo <pablomagub@gmail.com>
2015-11-13 00:52:00 +0100

    Signed-off-by: pablo <pablomagub@gmail.com>

--- Base_datos/src/basedatos/Datos.java
+++ Base_datos/src/basedatos/Datos.java
@@ -7,7 +7,7 @@ public class Datos {
 	Statement statement=null;
 	ResultSet resultados=null;
 	//parametron para la conexion
-	String url="jdbc:mysql://localhost/";
+	String url="jdbc:mysql://localhost/DAM";
 	String user="root";
 	String pw = "root";
 	try{
@@ -23,5 +23,30 @@ public class Datos {
 		System.out.println("conecion realizada con exito");
 		statement=(Sttement) conexion.createStatement();
 		resultados=statement.executeQuery("select * from tal")
+	}catch(SQLException e){
+		if(e.getSQLState().equals("28000"))
+			System.out.println("Error de autentificación");
+		else 
+			throw e;
+		e.printStackTrace();
+	}finally {
+		try{
+			if(conjuntoResultados!=null && !conjuntoResultados.isClosed())
+				conjuntoResultados.close();
+		}catch(SQLException e){
+			Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, e);
+		}
+		try{
+			if(instruccion!=null && !instruccion.isClosed())
+				instruccion.close();
+		}catch(SQLException e){
+			
+		}
+		try{
+			if(conexion!=null && !conexion.isClosed())
+				conexion.close();
+		}catch(SQLException e){
+			
+		}
 	}
 }
