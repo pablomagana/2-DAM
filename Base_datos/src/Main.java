664a9d6b709c922eb53b18601daf4fa80ee0b0ae
Pablo Magaña Ubeda <pablomagub@gmail.com>
2015-11-13 03:38:19 +0100

    Signed-off-by: Pablo Magaña Ubeda <pablomagub@gmail.com>

---
+++ Base_datos/src/Main.java
@@ -0,0 +1,9 @@
+import basedatos.Datos;
+public class Main {
+
+	public static void main(String[] args) {
+		Datos d=new Datos();
+		
+	}
+
+}
--- Base_datos/src/basedatos/Datos.java
+++ Base_datos/src/basedatos/Datos.java
@@ -7,46 +7,83 @@ public class Datos {
 	Statement statement=null;
 	ResultSet resultados=null;
 	//parametron para la conexion
-	String url="jdbc:mysql://localhost/DAM";
-	String user="root";
-	String pw = "root";
-	try{
-		//cargar el driver de conexion con la base de datos durante la ejecución
-		Class.forName("com.mysql.jdbc.Driver");
-	}catch(ClassNotFoundException e){
-		System.out.println("Error al cargar el controlador");
+	static String url="jdbc:mysql://localhost/DAM";
+	static String user="root";
+	static String pw = "root";
+	
+	public Datos(){
+		
 	}
 	
-	//crear la conexion con la base de datos mediante el driveManager
-	try{
-		conexion=DriverManager.getConnection(url,user,pw);
-		System.out.println("conecion realizada con exito");
-		statement=(Sttement) conexion.createStatement();
-		resultados=statement.executeQuery("select * from tal")
-	}catch(SQLException e){
-		if(e.getSQLState().equals("28000"))
-			System.out.println("Error de autentificación");
-		else 
-			throw e;
-		e.printStackTrace();
-	}finally {
+	public void cargarControlador(){
 		try{
-			if(conjuntoResultados!=null && !conjuntoResultados.isClosed())
-				conjuntoResultados.close();
-		}catch(SQLException e){
-			Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, e);
-		}
-		try{
-			if(instruccion!=null && !instruccion.isClosed())
-				instruccion.close();
-		}catch(SQLException e){
-			
-		}
-		try{
-			if(conexion!=null && !conexion.isClosed())
-				conexion.close();
-		}catch(SQLException e){
-			
+			//cargar el driver de conexion con la base de datos durante la ejecución
+			Class.forName("com.mysql.jdbc.Driver");
+		}catch(ClassNotFoundException e){
+			System.out.println("Error al cargar el controlador");
 		}
 	}
+	
+	public void introducirDatos(String[] arg){
+		//crear la conexion con la base de datos mediante el driveManager
+		try{
+			conexion=DriverManager.getConnection(url,user,pw);
+			System.out.println("conecion realizada con exito");
+			statement=(Statement) conexion.createStatement();
+			statement.executeQuery("insert into dam(asignatura,nombre,inicio,fin,entregada) values('"+arg[0]+"','"+arg[1]+"',"+arg[2]+","+arg[3]+","+arg[4]+")");
+		}catch(SQLException e){
+			if(e.getSQLState().equals("28000"))
+				System.out.println("Error de autentificación");
+			else 
+				throw e;
+			e.printStackTrace();
+		}finally {
+			try{
+				if(instruccion!=null && !instruccion.isClosed())
+					instruccion.close();
+			}catch(SQLException e){}
+			try{
+				if(conexion!=null && !conexion.isClosed())
+					conexion.close();
+			}catch(SQLException e){}
+		}
+	}
+	
+	public ArrayList<String> leerDatos(){
+		ArrayList<String> arrayListResultados=new ArrayList<String>();
+		//crear la conexion con la base de datos mediante el driveManager
+		try{
+			conexion=DriverManager.getConnection(url,user,pw);
+			System.out.println("conecion realizada con exito");
+			statement=(Statement) conexion.createStatement();
+			resultados=statement.executeQuery("insert into dam(asignatura,nombre,inicio,fin,entregada) values('"+arg[0]+"','"+arg[1]+"',"+arg[2]+","+arg[3]+","+arg[4]+")");
+			//iterar resultados
+			Iterator i=(String) resultados.iterator();
+			while(i.hasNext()){
+				//
+			}
+		}catch(SQLException e){
+			if(e.getSQLState().equals("28000"))
+				System.out.println("Error de autentificación");
+			else 
+				throw e;
+			e.printStackTrace();
+		}finally {
+			try{
+				if(conjuntoResultados!=null && !conjuntoResultados.isClosed())
+					conjuntoResultados.close();
+			}catch(SQLException e){
+				Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, "error al cerrar los resultados", e);
+			}
+			try{
+				if(instruccion!=null && !instruccion.isClosed())
+					instruccion.close();
+			}catch(SQLException e){}
+			try{
+				if(conexion!=null && !conexion.isClosed())
+					conexion.close();
+			}catch(SQLException e){}
+		}
+	}
+	
 }
