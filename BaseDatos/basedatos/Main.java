3b8c8689f23eb9b71e7cfe53735471089f0f7e5b
Pablo Magaña Ubeda <pablomagub@gmail.com>
2015-11-15 18:10:10 +0100

    Signed-off-by: Pablo Magaña Ubeda <pablomagub@gmail.com>

--- BaseDatos/basedatos/Main.java
+++ BaseDatos/basedatos/Main.java
@@ -3,6 +3,7 @@ package basedatos;
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println("pablo magaña");
 		Datos d=Datos.getInstance();
 		//String[] actividad=new String[]{"acceso base de datos","4a","2015-09-12","2015-09-21","0"};
 		d.introducirDatos(new String[]{"AAD","4a","2015-09-12","2015-09-21","true"});
